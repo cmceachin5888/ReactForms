8dbee8550f0426374a6b57a5450129dd0370938d
Cameron McEachin <cmceachin5888@gmail.com>
2023-05-25 20:44:15 -0400

    fixed error and added code for password

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Form = () => {
-  const [username, setUsername] = useState ('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   const handleSubmit = (event) => {
     event.preventDefault()
-    setUsername('')
     const username = event.target.username.value
-  
+    console.log(username, password)
+    setUsername('')
+    setPassword('')
+  }
+
   const handleChange = (event) => {
       setUsername(event.target.value)
-    }
-    console.log(username)
   }
+
   return (
     <div id='container'>
       <div id='navbar'>
@@ -20,8 +24,17 @@ const Form = () => {
       </div>
       <form className='logIn-form' onSubmit={handleSubmit}>
         <label htmlFor='username'>Username:</label>
-        <input type='text' name='username' value={username}
-        onChange={handleChange} />
+          <input 
+            type='text' 
+            name='username' 
+            value={username}
+            onChange={ handleChange } />
+        <label htmlFor='password'>Password:</label>
+          <input 
+            type='password' 
+            name='password' 
+            value={password} 
+            onChange={(event) => setPassword(event.target.value)} />
         <button type='submit'>Submit</button>
       </form>
     </div>
