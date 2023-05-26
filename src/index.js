01fb9940499f80adb2d70d79fd8eed878484fa2e
Shelem Oteo <shembot@Shelems-MacBook-Air.local>
2023-05-25 18:05:31 -0600

    added handlechange and functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Form = () => {
+  const [username, setUsername] = useState ('');
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setUsername('')
+    const username = event.target.username.value
+  
+  const handleChange = (event) => {
+      setUsername(event.target.value)
+    }
+    console.log(username)
+  }
   return (
     <div id='container'>
       <div id='navbar'>
         Form.js
       </div>
-      <form>
-        {/* your form fields here */}
+      <form className='logIn-form' onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' value={username}
+        onChange={handleChange} />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
